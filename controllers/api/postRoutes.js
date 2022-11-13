914d73102100b1acc9aeb2f134027fb9de347938
Chrisld50 <chrisld50@yahoo.com>
2022-11-13 15:40:41 -0600

    added to views updated routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,39 +1,37 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const {User, Comment, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', withAuth ,async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ['id', 'name', 'description', 'date_created','location','contact_name','pay','contact_email'],
+      order: [['date_created', 'DESC']],
+      include: [
+        { model: User, attributes: ['name'] }, {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id']} 
+      ],
+    });
+    res.status(200).json(postData.reverse());
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+//create route to create a new post
 router.post('/', withAuth, async (req, res) => {
-    try {
-      const newPost = await Post.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newPost);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  try {
+    console.log('test')
+    const newPost = await Post.create({
+      
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-  router.delete('/:id', withAuth, async (req, res) => {
-    try {
-      const postData = await Post.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
-  
-      if (!postData) {
-        res.status(404).json({ message: 'No post found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(postData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  module.exports = router;
-  
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
