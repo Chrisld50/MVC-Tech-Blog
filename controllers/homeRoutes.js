8e30c78d4b4b8641137cd02742fd4086ce221b6b
Chrisld50 <chrisld50@yahoo.com>
2022-11-13 20:34:05 -0600

    fixed models

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Comment, Post, User } = require('../models');
-const withAuth = require('../utils/auth');
+//const { Comment, Post, User } = require('../models');
+//const withAuth = require('../utils/auth');
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
@@ -13,46 +12,46 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'], 
-        }, 
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id']
-        } 
-      ],order: [
-        ['date_created', 'ASC'],
-    ],
-    });
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const postData = await Post.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'], 
+//         }, 
+//         {
+//           model: Comment,
+//           attributes: ['id', 'comment_text', 'post_id', 'user_id']
+//         } 
+//       ],order: [
+//         ['date_created', 'ASC'],
+//     ],
+//     });
 
-    // Serialize data so the template can read it
-    const posts = postData.map((posts) => posts.get({ plain: true }));
+//     // Serialize data so the template can read it
+//     const posts = postData.map((posts) => posts.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('posts', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     // Pass serialized data and session flag into template
+//     res.render('posts', { 
+//       posts, 
+//       logged_in: req.session.logged_in 
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.get('/create',withAuth, async (req,res)=>{
-  try{
-    res.render('create',{
-      logged_in: req.session.logged_in
-    });
-  }catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get('/create',withAuth, async (req,res)=>{
+//   try{
+//     res.render('create',{
+//       logged_in: req.session.logged_in
+//     });
+//   }catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/signup', async (req,res)=>{
   try{
@@ -62,22 +61,30 @@ router.get('/signup', async (req,res)=>{
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    }) 
-    res.render('login');
-  } else {
-    res.status(404).end();
-  }
-});
+// router.post('/logout', (req, res) => {
+//   if (req.session.logged_in) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     }) 
+//     res.render('login');
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
-router.get('/contact',withAuth, async (req,res)=>{
-  try{
-    res.render('contact');
-  }catch (err) {
-    res.status(500).json(err);
+router.get('/', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/') 
+    return
   }
-});
+  res.render('dashboard')
+})
+
+// router.get('/contact',withAuth, async (req,res)=>{
+//   try{
+//     res.render('contact');
+//   }catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
   module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,21 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const name = document.querySelector('#email-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (name && password) {
+  if (email && password) {
     const response = await fetch('/api/users/login', {
       method: 'post',
       body: JSON.stringify({
-        name,
+        email,
         password,
       }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
       console.table(response);
     } else {
       alert(response.statusText);
@@ -23,6 +23,4 @@ async function loginFormHandler(event) {
   }
 }
 
-document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
